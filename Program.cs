fa6dc6b67077e14ee1ad9aaef2e41a0561913902
Илья <relaxjk@inbox.ru>
2021-09-14 22:18:35 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lesson1_Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello ");
+            Console.Write("World ");
+            Console.Write("!!! ");
+            Console.WriteLine();
+            Console.WriteLine("Нажмите 'Enter' для завершения");
+            while (Console.ReadKey(true).Key != ConsoleKey.Enter);
+        }
+    }
+}
